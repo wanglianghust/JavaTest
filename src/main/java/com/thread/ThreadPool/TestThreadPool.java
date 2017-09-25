6c45ee38848902d8be7f50b27e3e45503e8c1937
wanglianghust <289148307@qq.com>
2017-09-25 16:10:28 +0800

    ThreadPool

--- src/main/java/com/thread/ThreadPool/TestThreadPool.java
+++ src/main/java/com/thread/ThreadPool/TestThreadPool.java
@@ -11,14 +11,18 @@ public class TestThreadPool {
     }
 
     private final static ThreadPoolExecutor threadPool ;
-    private static int corePoolSize=20;
-    private static int maximumPoolSize=200;
-    private static int workQueue=200;
+    private static int corePoolSize=20;//初始化线程大小
+    private static int maximumPoolSize=200;//最大化线程大小
+    private static int workQueue=200;//最大缓存队列大小
 
 
     static {
         threadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, 30, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<Runnable>(workQueue),
+                //ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
+                //ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
+                //ThreadPoolExecutor.DiscardOldestPolicy：丢弃队列最前面的任务，然后重新尝试执行任务（重复此过程）
+                //ThreadPoolExecutor.CallerRunsPolicy：由调用线程处理该任务
                 new ThreadPoolExecutor.AbortPolicy()
         );
     }
