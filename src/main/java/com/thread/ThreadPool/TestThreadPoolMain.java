33179803e55bf96aebdaf6f5f15394f0d1d8c0f1
wanglianghust <289148307@qq.com>
2017-09-25 16:11:31 +0800

    ThreadPool

--- src/main/java/com/thread/ThreadPool/TestThreadPoolMain.java
+++ src/main/java/com/thread/ThreadPool/TestThreadPoolMain.java
@@ -6,12 +6,9 @@ import com.thread.TestRunnable;
 public class TestThreadPoolMain {
 
     public static void main(String[] args)  {
-
-
-		TestRunnable testRunnable = new TestRunnable();
-		Thread thread = new Thread(testRunnable);
+        TestRunnable testRunnable = new TestRunnable();
+        Thread thread = new Thread(testRunnable);
         TestThreadPool.getThreadPool().execute(thread);
-
     }
 
 }
